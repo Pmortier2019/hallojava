75927c5ed11ad054279bda983653c3bbbbaf6a1b
Pmortier2019 <94837969+Pmortier2019@users.noreply.github.com>
2022-02-10 12:00:39 +0100

    Zonder deel 4

---
+++ src/Practicum.java
@@ -0,0 +1,31 @@
+class Practicum {
+    public static void main(String[]args){
+
+        for (int i = 1;i <=10; i ++){
+            System.out.println(i);
+        }
+        {int i = 1;
+            while (i <= 10) {
+                System.out.println(i++);
+        }
+            { for (int teller = 1; teller<= 10; teller++){
+
+                double x = Math.random();
+                System.out.println(x
+                );
+            }
+
+
+            }
+
+            {
+                for (int count = 1; count<= 10; count++){
+                    if (count % 2 == 0)
+                        System.out.println("s");
+                    else
+                        System.out.println("ss");}
+
+
+
+            }}
+}}
